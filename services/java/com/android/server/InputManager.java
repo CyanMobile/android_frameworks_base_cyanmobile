b5d55f3778a152dc05ff82f76c1591ef7df41a58
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2012-08-31 12:06:38 +0700

    DO NOT SYNC, WIP (PART4)

--- services/java/com/android/server/InputManager.java
+++ services/java/com/android/server/InputManager.java
@@ -360,7 +360,7 @@ public class InputManager {
         
         @SuppressWarnings("unused")
         public void notifyConfigurationChanged(long whenNanos) {
-            mWindowManagerService.sendNewConfiguration();
+            mWindowManagerService.mInputMonitor.notifyConfigurationChanged();
         }
         
         @SuppressWarnings("unused")
