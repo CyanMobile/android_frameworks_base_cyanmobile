9578198f5c27c12aac043be2344aeda596c3f8f3
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2013-04-05 07:19:45 +0700

    cleanup weather

--- core/java/com/android/internal/util/weather/YahooPlaceFinder.java
+++ core/java/com/android/internal/util/weather/YahooPlaceFinder.java
@@ -21,11 +21,13 @@ import android.net.Uri;
 
 public class YahooPlaceFinder {
 
-    private static final String YAHOO_API_BASE_URL = "http://query.yahooapis.com/v1/public/yql?q=select%20woeid%20from%20geo.placefinder%20where%20text%3D";
+    private static final String YAHOO_API_BASE_URL = "http://query.yahooapis.com/v1/public/yql?q=" +
+        Uri.encode("select woeid from geo.placefinder where text =");
 
     public static String reverseGeoCode(Context c, double latitude, double longitude) {
-        String url = YAHOO_API_BASE_URL + Uri.encode(String.format("\"%s %s\" and gflags=\"R\"", String.valueOf(latitude),
-                String.valueOf(longitude)));
+        String formattedCoordinates = String.format("\"%s %s\" and gflags=\"R\"",
+                String.valueOf(latitude), String.valueOf(longitude));
+        String url = YAHOO_API_BASE_URL + Uri.encode(formattedCoordinates);
         String response = new HttpRetriever().retrieve(url);
         if (response == null) {
             return null;
